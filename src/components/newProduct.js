96824574a917a4b0a842a2d4c9ba90ad2779bbce
joelees1 <leesj4@coventry.ac.uk>
2024-03-03 18:36:53 +0000

    product post
    added post for products, fixed image upload

--- src/components/newProduct.js
+++ src/components/newProduct.js
@@ -5,29 +5,54 @@ import { UploadOutlined } from '@ant-design/icons';
 const { Header, Content } = Layout;
 
 function NewProduct() {
-    const onFinish = (values) => {
+    const [api, contextHolder] = message.useMessage();
+
+    const onFinish = async (values) => {
         console.log('New product details:', values);
-        // Submit the new product details to the server
+
+        // Construct FormData object
+        const formData = new FormData();
+        formData.append('name', values.name);
+        formData.append('description', values.description);
+        formData.append('creator', values.creator);
+        formData.append('category_id', values.category_id);
+        formData.append('price', values.price);
+        formData.append('image_url', values.image_url.fileList[0].originFileObj);
+        console.log('Form data:', formData);
+
+        // Send a POST request to the server with the form data
+		fetch('http://localhost:3030/api/v1/products', {
+			method: "POST",
+			body: formData
+		})
+		.then(response => {
+			if (!response.ok) { // If the server responds with a bad HTTP status, throw an error.
+				return response.json().then(err => {
+					throw new Error(err.error || 'Something went wrong');
+				});
+			}
+			return response.json(); // If the response is OK, proceed.
+		})
+		.then(data => { // successful response
+			api.open({ message: 'Product Created', description: `Product has been created, redirecting...`, duration: 5, type: 'success' });
+			document.querySelector('form').reset(); // clear the form
+
+			// Redirect to the home page
+			setTimeout(() => {
+				window.location.href = '/';
+			}, 1500);
+		})
+		.catch(error => { // unsuccessful response, with error from server
+			console.error(error);
+			api.open({ message: 'Error', description: error.message, duration: 5, type: 'error' });
+		});
     };
 
-    // image upload validation
-    const props = {
-        beforeUpload: (file) => {
-            const isAllowed = file.type === 'image/png';
-            if (!isAllowed) {
-                message.error(`${file.name} is not a PNG file`, 5); // Display message for 5 seconds
-            }
-            return (isAllowed) || Upload.LIST_IGNORE;
-        },
-
-        onChange: (info) => {
-            console.log(info.fileList);
-        },
-    };
 
     return (
         <div className='auth-layout-container'>
             <Layout className='auth auth-page'>
+                {contextHolder}
                 <Header className='auth auth-header'>
                     <h1 style={{ fontWeight: "bold" }}>New Product</h1>
                 </Header>
@@ -61,7 +86,6 @@ function NewProduct() {
                             <Input placeholder='Enter creator'/>
                         </Form.Item>
 
-                        {/* add category_id validation */}
                         <Form.Item
                             hasFeedback
                             label="Category ID"
@@ -86,7 +110,7 @@ function NewProduct() {
                             name="image_url"
                             rules={[{ required: true, message: 'Image upload is required' }]}
                         >
-                            <Upload {...props}>
+                            <Upload accept="image/png, image/jpeg" maxCount={1} beforeUpload={() => false}>
                                 <Button icon={<UploadOutlined />}>Upload image</Button>
                             </Upload>
                         </Form.Item>
